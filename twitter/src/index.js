92b18e6f4e4c20209d222a131035de9fe47e68ac
pranavpatilsce <ompranav97@gmail.com>
2019-11-14 01:45:59 -0800

    finished sign in

--- twitter/src/index.js
+++ twitter/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/home/home.js';
+import Login from './pages/login/login.js';
+//import SignUp from './pages/signup/signup.js';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 
 import UserProfile from './components/profile/profile.js';
 //TODO: Add redux for isLoggedIn or not state
@@ -17,9 +18,12 @@ class Routes extends React.Component {
   return(
     <Router>
       <Switch>
-
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' component={Login} />
         <Route exact path='/profile' component={UserProfile} />
+        <Route exact path='/bookmarks' component={UserProfile} />
+        <Route exact path='/messages' component={UserProfile} />
+        <Route exact path='/list' component={UserProfile} />
+        <Route exact path='/home' component={Home} />
       </Switch>
     </Router>
     )
