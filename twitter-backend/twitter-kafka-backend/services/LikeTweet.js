fe73dae84adbbf5fad763bc5feb9fdd416894585
shubh08 <shubhamkumar567@gmail.com>
2019-11-17 18:02:56 -0800

    Added Liked Tweet and increase like counter bug fix

--- twitter-backend/twitter-kafka-backend/services/LikeTweet.js
+++ twitter-backend/twitter-kafka-backend/services/LikeTweet.js
@@ -6,10 +6,13 @@ function handle_request(msg, callback) {
 
 console.log("Inside like tweet msg----", msg)
 
-profileModel.updateOne({_id:msg._id,"tweets.tweetId": {$eq:mongoose.Types.ObjectId(msg.tweetId)}},{
+profileModel.updateOne({_id:msg._id,tweets:{$elemMatch:{tweetId: {$eq: mongoose.Types.ObjectId(msg.tweetId)}}}},
+{
 "$set":{
     $inc:{"tweet.$.likes":1},
-    $push:{"tweets.$.likedBy":msg.likedBy}}}
+    $push:{"tweets.$.likedBy":msg.likedBy}}},
+    {
+    tweets:{$elemMatch:{tweetId: {$eq: mongoose.Types.ObjectId(msg.tweetId)}}}}
 // profileModel.find({_id:msg._id,"tweets.tweetId": {$eq:mongoose.Types.ObjectId(msg.tweetId)}}
 ,function(err, user) {
 
