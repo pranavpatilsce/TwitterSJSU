007f6a46a77bea74665d28eaae296e5db531a57d
shubh08 <shubhamkumar567@gmail.com>
2019-11-19 02:43:37 -0800

    Added redis integration with getfollowers

--- twitter-backend/twitter-kafka-backend/server.js
+++ twitter-backend/twitter-kafka-backend/server.js
@@ -1,6 +1,21 @@
 var connection =  new require('./kafka/Connection');
+
+//Setup Redis
+const redis = require('redis')
+
+//Initialize Redis
+// create and connect redis client to local instance.
+const client = redis.createClient(6379,'twitterredis.cexqq1.ng.0001.use2.cache.amazonaws.com', {no_ready_check: true})
+ 
+// echo redis errors to the console
+client.on('error', (err) => {
+    console.log("Error " + err)
+});
+
+
 //topics files
 var Database=require('./Database');
+var RedisClient = require('./Redis')
 var AddProfile = require('./services/AddProfile.js');
 var SignInProfile=require('./services/SignInProfile');
 var UpdateProfile= require('./services/UpdateProfile');
