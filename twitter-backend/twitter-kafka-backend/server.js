2e90bb1736581747df37541404f8d6235ba92d4a
shubh08 <shubhamkumar567@gmail.com>
2019-11-19 02:52:15 -0800

    Removed redis reference from server,js

--- twitter-backend/twitter-kafka-backend/server.js
+++ twitter-backend/twitter-kafka-backend/server.js
@@ -1,16 +1,5 @@
 var connection =  new require('./kafka/Connection');
 
-//Setup Redis
-const redis = require('redis')
-
-//Initialize Redis
-// create and connect redis client to local instance.
-const client = redis.createClient(6379,'twitterredis.cexqq1.ng.0001.use2.cache.amazonaws.com', {no_ready_check: true})
- 
-// echo redis errors to the console
-client.on('error', (err) => {
-    console.log("Error " + err)
-});
 
 
 //topics files
