51a3b825323312b8a5798513d144294c5701b3be
aditya-edu <aditya.mantri@sjsu.edu>
2019-11-20 03:21:29 -0800

    database test

--- twitter-backend/twitter-kafka-backend/Database.js
+++ twitter-backend/twitter-kafka-backend/Database.js
@@ -1,7 +1,11 @@
 let mongoose = require('mongoose');
 
 const connectionString='mongodb+srv://root:root@cluster0-9j3qi.mongodb.net/twittertest?retryWrites=true&w=majority'
-mongoose.createConnection(connectionString,{ poolSize: 10, useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true} );
+mongoose.createConnection(connectionString,{ poolSize: 10, useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+.then(()=>console.log("Database connection success"))
+.catch(err=>{
+	console.error("Database connection error")
+})
 // Email, Pwd, Name, Bio, Location, Website,
 //  BirthDate, ProfileImage, CoverImage, Tweets(Array), Followers(Array),
 //  Following(Array), BookmarkedTweets (Array), Messages (Array)
