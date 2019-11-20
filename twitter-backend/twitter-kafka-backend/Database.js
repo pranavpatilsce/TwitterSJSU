1e8d03fc7116e69a8dbb1b2ddd3318c9c98a5391
Manav Rajvanshi <manavraj96@gmail.com>
2019-11-19 23:37:32 -0800

    Changed the DB to twittertest

--- twitter-backend/twitter-kafka-backend/Database.js
+++ twitter-backend/twitter-kafka-backend/Database.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
-const connectionString='mongodb+srv://root:root@cluster0-9j3qi.mongodb.net/twitter?retryWrites=true&w=majority'
+const connectionString='mongodb+srv://root:root@cluster0-9j3qi.mongodb.net/twittertest?retryWrites=true&w=majority'
 mongoose.connect(connectionString,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true} );
 // Email, Pwd, Name, Bio, Location, Website,
 //  BirthDate, ProfileImage, CoverImage, Tweets(Array), Followers(Array),
@@ -21,7 +21,8 @@ var profileSchema = new mongoose.Schema({
 	following:Array,
 	bookmarkedTweets:Array,
 	chats:Array,
-	likedTweets: Array
+	likedTweets: Array,
+	userHandle: String
 
 })
 var profileModel = mongoose.model('profile', profileSchema);
