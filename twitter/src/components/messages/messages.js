2cda149193eeb34096b6c3a113522b26c849cdde
pranavpatilsce <ompranav97@gmail.com>
2019-12-03 04:20:11 -0800

    chat send works

--- twitter/src/components/messages/messages.js
+++ twitter/src/components/messages/messages.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button} from 'reactstrap';
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Form, FormGroup, Label, Input } from 'reactstrap';
 import logo from '../../svg/logo.svg';
 import axios from 'axios';
 import './messages.css';
@@ -15,44 +15,106 @@ import Kalyani from '../../svg/Kalyani.jpeg';
 import Mukesh from '../../svg/Mukesh.jpeg';
 import Kartik from '../../svg/Kartik.png';
 
-const otherTweets = [
-  {
-    chatId: "12345",
-    message: "CMPE273 tweet",
-    time: "02:16:57",
-    date: "Jul 23 2005",
-    name: "Mukesh Mogal",
-    userHandle: "@handle",
-    image: Mukesh
-  },{
-    chatId: "123456",
-    message: "CMPE273 tweet",
-    time: "02:16:57",
-    date: "Jul 23 2005",
-    name: "Kalyani Deshmukh",
-    userHandle: "@handle2",
-    image: Kalyani
-  },{
-    chatId: "123457",
-    message: "CMPE273 tweet",
-    time: "02:16:57",
-    date: "Jul 23 2005",
-    name: "Kartik Ulmarkar",
-    userHandle: "@handle3",
-    image: Kartik
-  }
-];
 
-const globalSomething = [];
+const SendMessageModal = (props) => {
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  console.log(props);
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+const sendMessage=()=>{
+
+  console.log("inside handle submit");
+  //this.props.sendMessage(this.state.message)
+  let data2 =
+  {
+    "chatId" : props.chat_id,
+    "senderId" : localStorage.getItem('id'),
+    "receiverId" : props.receiver_id,
+    "message" : document.getElementById('sendChatMessage').value
+  }
+  axios.post('/messages/addMessageToChat', data2)
+    .then((response) => {
+        console.log('response ok',response)
+        console.log("Status Code : ", response);
+
+        if(response.data!="error")
+        {
+            alert("Message sent");
+        }
+
+        if(response.data=="error")
+        {
+            alert("Invalid credentials");
+        }
+
+    })
+    .catch (response => {
+        alert("Invalid");
+        this.setState({});
+      }
+    )
+
+
+  // let data={
+  //   "senderId" : localStorage.getItem('id'),
+  //   "receiverId" : localStorage.getItem('otherUserId'),
+  //   "message": document.getElementById('sendChatMessage').value,
+  //   "senderHandle" :localStorage.getItem('userHandle'),
+  //   "receiverHandle" :localStorage.getItem('otherUserHandle')
+  // }
+  // axios.defaults.withCredentials=true
+  // axios.post('/messages/createChat',data)
+  // .then((response)=>{
+  //   console.log('create cht resp',response.data)
+  //   if(localStorage.getItem('chats')==null)
+  //     localStorage.setItem('chats',response.data._id)
+  //   else
+  //     localStorage.setItem('chats',localStorage.getItem('chats')+","+response.data._id)
+  //   window.location.reload()
+  // })
+  // .catch((err)=>{
+  //   console.log('err create chat',err)
+  // })
+}
+  return (
+    <span>
+      <Button color="success" onClick={toggle}>Send Message</Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        {/* <ModalHeader toggle={toggle}>Message</ModalHeader> */}
+        <ModalBody>
+        <Form>
+      <FormGroup row>
+        <Label for="exampleEmail" sm={2}>Message</Label>
+        <Col sm={10}>
+          <Input type="email" name="email" id="sendChatMessage" placeholder="send a message"  />
+        </Col>
+      </FormGroup>
+      </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={sendMessage}>Send</Button>
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </span>
+  );
+}
 
 class MessageCard extends React.Component{
 
   render() {
+      console.log(this.props.chatList);
 
       return(
         <div className = "MessagesCard">
-          {/* {globalSomething.map(message => {
-            return(*/}
+          {this.props.chatList.map(chat => {
+            return(
                 <div className="Messages-Card-indi"> {/*href="/chat"*/}
                 <div>
                     <div className="Messages-Image">
@@ -62,18 +124,20 @@ class MessageCard extends React.Component{
                     <div className="Messages-Card-Body">
                       <br/>
                       <div className="Messages-Card-Body-Content">
-                        <Button color="success"><p>Read Msgs</p></Button>
+                        <SendMessageModal chat_id={chat._id} receiver_id={chat.users[0] == localStorage.getItem('id')? chat.users[1]: chat.users[0]}/>
                         <h5 className="Messages-Card-Body-Name"></h5>
-                        <p className="Messages-Card-Body-Handle">{/*{_id.userHandle}*/}</p>
+                        <p className="Messages-Card-Body-Handle">{
+                          chat.userHandles[0] == localStorage.getItem('userHandle')? chat.userHandles[1]: chat.userHandles[0]
+                        }</p>
                         <p className="Messages-Card-Body-Date">{/*{messageList.date}*/}</p>
-                        <Button color="danger"><p>Message</p></Button>
+                        <Button color="danger"><p>View Chat</p></Button>
                       </div>
                       <br/>
                     </div>
                 </div>
-                </div>
-            {/*  )
-           })} */}
+              </div>
+             )
+           })}
         </div>
       )
     }
@@ -84,7 +148,7 @@ class Messages extends React.Component {
   constructor(props) {
     super(props)
       this.state = {
-        messagesList: globalSomething
+        chatList: []
       };
 
   }
@@ -105,20 +169,14 @@ class Messages extends React.Component {
               alert("Invalid credentials");
           }
 
-          this.setState({messagesList: response.data});
+          this.setState({chatList: response.data}, ()=>{
+            console.log(this.state.chatList);
+          });
+
       })
       .catch (response => {
           alert("Invalid");
           this.setState({});
-      })
-      .then(response => {
-          this.setState({
-             messagesList: [response.data]
-          });
-
-      })
-      .catch(response => {
-          alert("Invalid");
       })
   }
 
@@ -136,7 +194,7 @@ class Messages extends React.Component {
       </div>
       <div type="button" className="Messages-Messages">
         <div type="button" className="Messages-Messages-Card">
-          <MessageCard/> {/*messagesList={this.state.messagesList}*/}
+          <MessageCard chatList={this.state.chatList}/>
         </div>
       </div>
       <div className="Messages-RightSide">
