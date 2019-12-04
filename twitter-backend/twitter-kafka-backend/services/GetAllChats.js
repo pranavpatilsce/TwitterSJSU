7c588a87a6201761ff95509816f28c185ebc6aae
Shubham Kumar <shubhamkumar567@gmail.com>
2019-12-04 13:58:51 -0800

    Added viewDate empty array during tweet creation

--- twitter-backend/twitter-kafka-backend/services/GetAllChats.js
+++ twitter-backend/twitter-kafka-backend/services/GetAllChats.js
@@ -5,18 +5,21 @@ const Chat = database.Chat;
 function handle_request(msg, callback) {
     console.log("**********************");
     console.log("Inside getAllChats kafka backend, msg: ", typeof(msg) );
-    callback(null, true);
-//   let chatIds = msg.split(',');
-//     console.log("-------",chatIds,"---------------------------------------------------");
-//     Chat.find( {_id: {$in:chatIds}}, function(err, chats){
-//         if(err){
-//             console.log("Error in Kafka Backend -> GetAllChat");
-//             callback(err, null);
-//         }else{
-//             console.log("Sending All chats to backend from kafka backend");
-//             callback(null, chats);
-//         }
-//     })
+    if(msg==null || msg=='')
+    {
+        callback(err, null);
+    }
+  let chatIds = msg.split(',');
+    console.log("-------",chatIds,"---------------------------------------------------");
+    Chat.find( {_id: {$in:chatIds}}, function(err, chats){
+        if(err){
+            console.log("Error in Kafka Backend -> GetAllChat");
+            callback(err, null);
+        }else{
+            console.log("Sending All chats to backend from kafka backend");
+            callback(null, chats);
+        }
+    })
 
 };
 
