1a1a06c1db4827f26822c76f5b0d20ca52bae95c
Shubham Kumar <shubhamkumar567@gmail.com>
2019-12-04 04:24:22 -0800

    Update GetAllTweets.js

--- twitter-backend/twitter-kafka-backend/services/GetAllTweets.js
+++ twitter-backend/twitter-kafka-backend/services/GetAllTweets.js
@@ -5,42 +5,42 @@ const mongoose=require('mongoose')
 function handle_request(msg, callback) {
     console.log("**********************");
     console.log("Inside getAllTweets kafka backend, msg: ", msg);
-    
-    Profile.findById({_id:msg}, function(err, profile){
-        if(err){
-            console.log("Error in Kafka Backend -> getAllTweets");
-            callback(err, null);
-        }else{
-            console.log("INSIDE getAllTweets KAFKA BACKEND");
-            let tweets =profile.tweets; // mongoose.Types.ObjectId(msg.tweetId)
-            console.log('Profile Followers ----------->',profile)
-            let profileIDS = [];
-            for(let profilee of profile.followers ){
-                profileIDS.push(mongoose.Types.ObjectId(profilee));
+     callback(null, success);
+//     Profile.findById({_id:msg}, function(err, profile){
+//         if(err){
+//             console.log("Error in Kafka Backend -> getAllTweets");
+//             callback(err, null);
+//         }else{
+//             console.log("INSIDE getAllTweets KAFKA BACKEND");
+//             let tweets =profile.tweets; // mongoose.Types.ObjectId(msg.tweetId)
+//             console.log('Profile Followers ----------->',profile)
+//             let profileIDS = [];
+//             for(let profilee of profile.followers ){
+//                 profileIDS.push(mongoose.Types.ObjectId(profilee));
             
-        }
+//         }
 
-        console.log('Profile IDS are ----------->',profileIDS)
+//         console.log('Profile IDS are ----------->',profileIDS)
 
-            Profile.find({_id:{ $in: profile.following}}, function(err, profileAll){
-                if(err){
-                    console.log("Error in Kafka Backend -> getAllTweets");
-                    callback(err, null);
-                }else{
-                    console.log("INSIDE getAllTweets KAFKA BACKEND get all profiles",profileAll);
+//             Profile.find({_id:{ $in: profile.following}}, function(err, profileAll){
+//                 if(err){
+//                     console.log("Error in Kafka Backend -> getAllTweets");
+//                     callback(err, null);
+//                 }else{
+//                     console.log("INSIDE getAllTweets KAFKA BACKEND get all profiles",profileAll);
 
-                    for(let profile of profileAll ){
-                        tweets.push(...profile.tweets);
+//                     for(let profile of profileAll ){
+//                         tweets.push(...profile.tweets);
                     
-                }
+//                 }
     
         
-                    callback(null, tweets);  
-                }
-            });
+//                     callback(null, tweets);  
+//                 }
+//             });
 
-        }
-    });
+//         }
+//     });
 
 };
 exports.handle_request = handle_request;
