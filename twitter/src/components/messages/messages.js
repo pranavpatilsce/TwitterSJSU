fd4cde77976a2f4e8f31724368b2711d82ac9ee8
Nachiket Trivedi <nachiket5197@gmail.com>
2019-12-04 14:04:35 -0800

    tyfctyf

--- twitter/src/components/messages/messages.js
+++ twitter/src/components/messages/messages.js
@@ -277,7 +277,7 @@ class Messages extends React.Component {
 
       })
       .catch (response => {
-          alert("Invalid");
+          alert("Invalid--");
           this.setState({});
       })
   }
@@ -329,3 +329,363 @@ class Messages extends React.Component {
 }
 
 export default Messages;
+// import React, {useState} from 'react';
+// import {Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Form, FormGroup, Label, Input } from 'reactstrap';
+
+
+// import logo from '../../svg/logo.svg';
+// import axios from 'axios';
+// import './messages.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Navigation from '../../nav/globalNav.js';
+// import RightSide from '../../components/search/search.js';
+// import MessageBox from './chatBox.js';
+// //import MessageCard from './chatList.js';
+// import './chatList.css';
+
+
+// const SendMessageModal = (props) => {
+//   const {
+//     buttonLabel,
+//     className
+//   } = props;
+
+//   console.log(props);
+
+//   const [modal, setModal] = useState(false);
+
+//   const toggle = () => setModal(!modal);
+// const sendMessage=()=>{
+
+//   console.log("inside handle submit");
+//   //this.props.sendMessage(this.state.message)
+//   let data2 =
+//   {
+//     "chatId" : props.chat_id,
+//     "senderId" : localStorage.getItem('id'),
+//     "receiverId" : props.receiver_id,
+//     "message" : document.getElementById('sendChatMessage').value
+//   }
+//   axios.post('/messages/addMessageToChat', data2)
+//     .then((response) => {
+//         console.log('response ok',response)
+//         console.log("Status Code : ", response);
+
+//         if(response.data!="error")
+//         {
+//             window.location.reload()
+//         }
+
+//         if(response.data=="error")
+//         {
+//             alert("Invalid credentials");
+//         }
+
+//     })
+//     .catch (response => {
+//         alert("Invalid");
+//         this.setState({});
+//       }
+//     )
+
+
+//   // let data={
+//   //   "senderId" : localStorage.getItem('id'),
+//   //   "receiverId" : localStorage.getItem('otherUserId'),
+//   //   "message": document.getElementById('sendChatMessage').value,
+//   //   "senderHandle" :localStorage.getItem('userHandle'),
+//   //   "receiverHandle" :localStorage.getItem('otherUserHandle')
+//   // }
+//   // axios.defaults.withCredentials=true
+//   // axios.post('/messages/createChat',data)
+//   // .then((response)=>{
+//   //   console.log('create cht resp',response.data)
+//   //   if(localStorage.getItem('chats')==null)
+//   //     localStorage.setItem('chats',response.data._id)
+//   //   else
+//   //     localStorage.setItem('chats',localStorage.getItem('chats')+","+response.data._id)
+//   //   window.location.reload()
+//   // })
+//   // .catch((err)=>{
+//   //   console.log('err create chat',err)
+//   // })
+// }
+//   return (
+//     <span>
+//       <Button color="success" onClick={toggle}>Send Message</Button>
+//       <Modal isOpen={modal} toggle={toggle} className={className}>
+//         {/* <ModalHeader toggle={toggle}>Message</ModalHeader> */}
+//         <ModalBody>
+//         <Form>
+//       <FormGroup row>
+//         <Label for="exampleEmail" sm={2}>Message</Label>
+//         <Col sm={10}>
+//           <Input type="email" name="email" id="sendChatMessage" placeholder="send a message"  />
+//         </Col>
+//       </FormGroup>
+//       </Form>
+//         </ModalBody>
+//         <ModalFooter>
+//           {/* <ReceiveModal/> */}
+//           <Button color="primary" onClick={sendMessage}>Send</Button>
+//           <Button color="secondary" onClick={toggle}>Cancel</Button>
+//         </ModalFooter>
+//       </Modal>
+//     </span>
+//   );
+// }
+
+
+// const ReceiveModal = (props) => {
+
+
+//   const [modal, setModal] = useState(false);
+
+//   const toggle = () => setModal(!modal);
+
+//   console.log('props-->',props);
+//   let chatArr=props['gettingchat']
+//   let handles=props['handles']
+//   let divToggle='';
+//   let dispChat
+//   if(chatArr!=undefined){
+//    dispChat=chatArr.map((item)=>{
+//     console.log('item',item['senderId'])
+//     console.log('loc',localStorage.getItem('id'))
+//     divToggle=(item['senderId']==localStorage.getItem('id')?divToggle='right':divToggle='left')
+//     console.log('divToggle',divToggle)
+//     return(
+//       <div>
+//         {/* <div className={divToggle}><i>{item['time']}</i> |</div> */}
+//         <div className={divToggle}>{item['message']}</div>
+//         <hr/>
+//         <br/>
+//       </div>
+//     )
+//   })
+// }
+//   return (
+//     <div>
+//       <Button color="danger" onClick={toggle}>View Chat</Button>
+//       <Modal isOpen={modal} toggle={toggle} >
+//         {/* <ModalHeader toggle={toggle}>{handles[0]} | {handles[1]}</ModalHeader> */}
+//         <ModalBody>
+//           {dispChat}
+//         </ModalBody>
+//         <ModalFooter>
+//           <Button color="secondary" onClick={toggle}>Cancel</Button>
+//         </ModalFooter>
+//       </Modal>
+//     </div>
+//   );
+// }
+
+// class MessageCard extends React.Component{
+
+//   constructor(props){
+//     super(props)
+
+//     //this.getChat = this.getChat.bind(this);
+//     this.state = {
+//       gettingchat: [],
+//     }
+//     let chatId=localStorage.getItem('chats')
+//     console.log('chatId here', chatId);
+//     axios.defaults.withCredentials=true
+//     axios.get(`/messages/getChat/${chatId}`)
+//       .then((response) => {
+//           //console.log('response ok',response)
+//           console.log("All Chats:", response.data);
+
+//           if(response.data=="error")
+//           {
+//               alert("Invalid credentials");
+//           }
+
+//           this.setState({gettingchat: response.data}, ()=>{
+//             console.log(this.state.gettingchat);
+//           });
+
+//       })
+//       .catch (response => {
+//           alert("Invalid");
+//           this.setState({});
+//       })
+
+//   }
+
+//   getChat(chatId){
+//     console.log('chatId here', chatId);
+//     axios.defaults.withCredentials=true
+//     axios.get(`/messages/getChat/${chatId}`)
+//       .then((response) => {
+//           //console.log('response ok',response)
+//           console.log("All Chats:", response.data);
+
+//           if(response.data=="error")
+//           {
+//               alert("Invalid credentials");
+//           }
+
+//           this.setState({gettingchat: response.data}, ()=>{
+//             console.log(this.state.gettingchat);
+//           });
+
+//       })
+//       .catch (response => {
+//           alert("Invalid");
+//           this.setState({});
+//       })
+//   }
+
+//   render() {
+//       console.log(this.props.chatList);
+//       let dispAll=null
+//       if(this.props.chatList!=null)
+//       {
+//         dispAll=this.props.chatList.map(chat => {
+//           console.log('=>',chat)
+//           return(
+//               <div className="Messages-Card-indi"> {/*href="/chat"*/}
+//               <div>
+//                   <div className="Messages-Image">
+//                     <br/>
+//                     {/*<img className="Messagesimage" src={_id.image}/>*/}
+//                   </div>
+//                   <div className="Messages-Card-Body">
+//                     <br/>
+//                     <div className="Messages-Card-Body-Content">
+//                       <SendMessageModal chat_id={chat._id} receiver_id={chat.users[0] == localStorage.getItem('id')? chat.users[1]: chat.users[0]}/>
+//                       <h5 className="Messages-Card-Body-Name"></h5>
+//                       <p className="Messages-Card-Body-Handle">{
+//                         chat.userHandles[0] == localStorage.getItem('userHandle')? chat.userHandles[1]: chat.userHandles[0]
+//                       }</p>
+//                       <p className="Messages-Card-Body-Date">{/*{messageList.date}*/}</p>
+//                       <ReceiveModal color="danger" gettingchat={chat['messages']} handles={chat['userHandles']}/>
+//                     </div>
+//                     <br/>
+//                   </div>
+//               </div>
+//             </div>
+//            )
+//          })
+//       }
+//       return(
+//         <div className = "MessagesCard">
+//           {dispAll}
+//         </div>
+//       )
+//     }
+// }
+
+// class MessageList extends React.Component {
+
+//     constructor(props){
+//       super(props)
+
+//     }
+
+//     render() {
+//         return (
+//           <div>
+
+//             {/*<ul className="message-list">
+//                   {this.props.chat.map(message => {
+//                       return (
+//                         <li className="message">
+//                           <div>{message.message}</div> {/* this was sender id
+//                         </li>
+//                       )
+//                   })}
+//               </ul>*/}
+//           </div>
+//        )
+//     }
+// }
+
+// class Messages extends React.Component {
+
+//   constructor(props) {
+//     super(props)
+//       this.state = {
+//         chatList: [],
+//         chat: []
+//       };
+//   }
+
+//   componentWillMount(){
+//     let data = {
+//       chats: localStorage.getItem('chats')
+//       // chats: "5de48e956528003d3887b7b3,5de491e4aff08e0c8cdab48e"
+//     }
+//     console.log('data',data)
+//     axios.defaults.withCredentials=true
+//     axios.post('/messages/getAllChats', data)
+//       .then((response) => {
+//           console.log('response ok',response)
+//           console.log("All Chats:", response.data);
+//           alert('ad')
+
+//           if(response.data=="error")
+//           {
+//               alert("Invalid credentials");
+//           }
+//           // localStorage.setItem('chats',response.data)
+//           this.setState({chatList: response.data}, ()=>{
+//             console.log(this.state.chatList);
+//           });
+
+//       })
+//       .catch (response => {
+//           alert("Invalid");
+//           this.setState({});
+//       })
+//   }
+
+//   render(){
+//     let a;
+//     if(this.state.chat.length == 0 || this.state.chat == undefined){
+//       a = <div></div>;
+//     }else{
+//       a = <div className="Messages-RightSide">
+//             <div className="chat-div">
+//               <MessageList chat={this.state.chat} />
+//               {/*<SendMessageForm sendMessage={this.sendMessage} />*/}
+//             </div>
+//           </div>
+//     }
+//   return (
+//     <div className="Messages">
+
+//       <div className="Messages-Navigation">
+//         <Navigation />
+//       </div>
+//       <div className="MessagesBar">
+//         <div>
+//           <Button className="MessagesBarTitle"><h3>Messages</h3></Button>
+//         </div>
+//       </div>
+//       <div type="button" className="Messages-Messages">
+//         <div type="button" className="Messages-Messages-Card">
+//           <MessageCard chatList={localStorage.getItem('cards')} chat={this.state.chat}/>
+//         </div>
+//       </div>
+//       {/*
+//       <div type="button" className="Messages-RightSide">
+//           <div className="Messages-RightSide-PreLoad">
+//             <MessageBox/>
+//           </div>
+//       </div>*/}
+//       <div className="Messages-RightSide">
+//           <div className="chat-div">
+//             <MessageList  />
+//             {/*<SendMessageForm sendMessage={this.sendMessage} />*/}
+//           </div>
+//       </div>
+
+//     </div>
+//   )
+//   }
+// }
+
+// export default Messages;
