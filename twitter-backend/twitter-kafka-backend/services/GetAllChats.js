a41ea426b6a6ac3deeb92c7b87e1cab5577737b2
Manav Rajvanshi <manavraj96@gmail.com>
2019-12-04 16:15:06 -0800

    ASdfasd

--- twitter-backend/twitter-kafka-backend/services/GetAllChats.js
+++ twitter-backend/twitter-kafka-backend/services/GetAllChats.js
@@ -4,23 +4,22 @@ const Chat = database.Chat;
 
 function handle_request(msg, callback) {
     console.log("**********************");
-    callback(null, true);
     console.log("Inside getAllChats kafka backend, msg: ", typeof(msg) );
-//     if(msg==null || msg=='')
-//     {
-//         callback(err, null);
-//     }
-//   let chatIds = msg.split(',');
-//     console.log("-------",chatIds,"---------------------------------------------------");
-//     Chat.find( {_id: {$in:chatIds}}, function(err, chats){
-//         if(err){
-//             console.log("Error in Kafka Backend -> GetAllChat");
-//             callback(err, null);
-//         }else{
-//             console.log("Sending All chats to backend from kafka backend");
-//             callback(null, chats);
-//         }
-//     })
+    if(msg==null || msg=='')
+    {
+        callback(err, null);
+    }
+  let chatIds = msg.split(',');
+    console.log("-------",chatIds,"---------------------------------------------------");
+    Chat.find( {_id: {$in:chatIds}}, function(err, chats){
+        if(err){
+            console.log("Error in Kafka Backend -> GetAllChat");
+            callback(err, null);
+        }else{
+            console.log("Sending All chats to backend from kafka backend");
+            callback(null, chats);
+        }
+    })
 
 };
 
